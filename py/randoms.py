1893e72299738e17f4f454371f0e7db8f8e93e6f
ChangHoon Hahn <chh327@nyu.edu>
2018-04-10 10:54:05 -0700

    method for constructing randoms implemented and very loosely tested; dndz redshift assignment still requires implementing

--- py/randoms.py
+++ py/randoms.py
@@ -5,6 +5,7 @@ module for generating randoms catalogs used in cosmological analyses
 
 Authors: 
     ChangHoon Hahn -- changhoonhahn@lbl.gov
+    Sukdeep Singh 
     Martin White 
 
 '''
@@ -12,27 +13,54 @@ import numpy as np
 import h5py 
 import warnings
 
-import nbodykit.lab as    *
 from astropy.io import fits
+from astropy.table import Table
 
 from desimodel.io  import load_pixweight
 from desimodel     import footprint
-from astropy.table import Table
-from astropy.io    import fits
 
 
 class Randoms(object): 
-    ''' add description of the object here. 
+    ''' class object for constructing random catalogs used in 
+    cosmological analyses (e.g. measuring the two-point correlation 
+    function). 
     '''
     def __init__(self): 
         pass 
 
     def make(self, Nr, zs=None, dndz=None, seed=None, file=None): 
-        ''' code for constructing random catalogs. 
+        ''' construct random catalogs with `Nr` randoms with the DESI 
+        footprint. First (RA, Dec) values are uniformly sampled from a 
+        box that conservatively covers the DESI footprint. The (RA, Dec) 
+        values are then checked to ensure they are within the DESI 
+        footprint. If redshift values are desired (i.e. kwargs zs or dndz
+        is specified) redshifts are assigned to the random points. 
+
+        parameters
+        ----------
+        Nr : int
+            Number of random galaxies in the catalog
+
+        zs : (optional) ndarray
+            if specified the random (RA, Dec) values within the DESI
+            footprint are assigned redshifts randomly sampled from this 
+            array. 
+
+        dndz : (optional) ndarray
+            instead of zs, dndz can be provided and the random (RA, Dec) 
+            values within the DESI footprint are assigned redshifts randomly 
+            to reproduce the dndz.   
+
+        seed : (optional) int
+            random seed
+
+        file : (optional) str
+            if specified, the random catalog will be saved to file. At the
+            moment only .fits and .hdf5 can be specified.   
         '''
         if zs is None and dndz is None: 
             msg = "No redshifts or dn/dz provided \n code will not return redshifts"
-            raise warnings.warn(msg) 
+            warnings.warn(msg) 
         if zs and dndz: 
             raise ValueError("specify either zs or dndz, not both") 
 
@@ -42,7 +70,7 @@ class Randoms(object):
         # to improve
         rr = np.random.RandomState(seed) # random seed
 
-        ra, dec = rr.uniform(size=(4.*Nr,2)) 
+        ra, dec = rr.uniform(size=(2, 4*Nr)) 
     
         ra *= 360.
         dec *= 90.
@@ -58,11 +86,13 @@ class Randoms(object):
         w = (w[keep])[:Nr]
 
         if zs is None and dndz is None: 
+            # save to astropy table 
+            tb = Table([ra, dec, w], names=('ra', 'dec', 'weight'))  
+
             # no redshifts needed output now 
-            if file is not  None: # save to file 
-                self.write2file(file, np.vstack([ra, dec, w]).T, 
-                        cols=['ra', 'dec', 'weight'])
-            return np.vstack([ra, dec, w]).T
+            if file is not None: # save to file 
+                self._write2file(file, tb)
+            return tb
     
         # generate redshifts for galaxies 
         if zs is not None: 
@@ -73,7 +103,7 @@ class Randoms(object):
             raise NotImplementedError
 
         if file is not None: 
-            self.write2file(file, np.vstack([ra, dec, z, w]).T, 
+            self._write2file(file, np.vstack([ra, dec, z, w]).T, 
                     cols=['ra', 'dec', 'z', 'weight'])
         return np.vstack([ra, dec, z, w]).T          
 
@@ -90,19 +120,33 @@ class Randoms(object):
         weights   = pixweight[healpix] # weights=1 in footprint
         return weights
 
-    def write2file(self, name, data, cols=None): 
+    def _write2file(self, name, tbl): 
         ''' write data to specified file 
         '''
-        assert len(cols) == data.shape[1] 
-
         # check file type 
-        ftype = name.strip('.')[-1].lower()
+        ftype = name.split('.')[-1].lower()
         if ftype not in ['fits', 'hdf5']: 
-            raise NotImplementedError("Only fits and hdf5 files supported at the moment") 
+            msg = ''.join([ftype, ' not supported at the moment', '\n', 'Only fits and hdf5 files supported at the moment']) 
+            raise NotImplementedError(msg) 
 
         if ftype == 'fits': # fits file 
+            # header specifies the columns; this can easily 
+            # be expanded to fit more things 
+            tbl.meta['COMMENTS'] = 'random catalog generated from randoms.py' # some comment here 
+            tbl.write(name, format='fits') 
 
-
-        elif ftype == 'hdf5': 
-
+        elif ftype == 'hdf5':
+            f = h5py.File(name, 'w')
+            # set metadata 
+            f.attrs['COMMENTS'] = 'random catalog generated from randoms.py' # some comment here 
+            # save each column 
+            for i, col in enumerate(tbl.colnames): 
+                f.create_dataset(col, data=tbl.columns[i])
+            f.close() 
         return None 
+
+
+if __name__=="__main__": 
+    rand = Randoms() 
+    #rand.make(10000, seed=1, file='test.fits') 
+    rand.make(10000, seed=1, file='test.hdf5') 
